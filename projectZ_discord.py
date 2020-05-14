2c72a3bbedebfc64a26c266a3b1b60eecacca379
stygiang <64345666+stygiang@users.noreply.github.com>
2020-05-14 14:32:45 -0400

    Update projectZ_discord.py

--- projectZ_discord.py
+++ projectZ_discord.py
@@ -15,16 +15,18 @@ async def on_member_join(member):
 @client.command()
 async def create(ctx):
     await client.create_guild("name")
-@client.command()
-async def ping(ctx):
-    await ctx.send("hello")
-    await client.create_guild("text1")
+# @client.command()
+# async def ping(ctx):
+#     await ctx.send("hello")
+#     await client.create_guild("text1")
 @client.command()
 async def about_server(x):
     await x.send("this is a testing server")
 @client.command()
 async def daily_challenge(x):
+    # challenges that will be displayed when .daily_challenge command is used
     challenges = ["1)create a shop \n[1]-shop has inventory \n[2]-shopper has cart \n[3]-shop has atleast 15 items",
                   "2)Write a function that takes an integer minutes and converts it to seconds.\n Examples:\nconvert(5) ➞ 300\nconvert(3) ➞ 180\nconvert(2) ➞ 120"]
-    await x.send(f"{challenges[1]}")
+    # will choose random challenge from list then send it to server
+    await x.send(f"{random.choice(challenges)}")
 client.run('NzEwNTA3ODUxNzU1NTUyNzc4.Xr1hjA.beJQLF1oQ3F4y0N51nsxuo4uzZQ')
